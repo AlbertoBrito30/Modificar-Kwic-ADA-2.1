0ec0befead56bed096bbcfcb05fe2a9ba6cf993a
Onigiri sempai <betitobrito30@icloud.com>
2026-08-28 02:00:09 -0600

    Falta moverle a la huevada de las colas y sacar la lista, falta leer txt, view y controlador

--- src/main/java/com/mycompany/kwic/model/LogicKwic.java
+++ src/main/java/com/mycompany/kwic/model/LogicKwic.java
@@ -11,16 +11,18 @@ import java.util.*;
  * @author betit
  */
 public class LogicKwic {
+    
+    //letras a ignorar
+    private final Set<String> palabrasIgnoradas = new HashSet<>(Arrays.asList("de", "el", "la", "los", "las", "a", "en", "y"));
 
     //variables de estructuras
     private final Scanner sc;
-    private List<String> listaPalabras;
+    private List<List<String>> listaPalabras;
 
     //variables
-    private boolean var = true;
     private String texto = "";
 
-    public LogicKwic(Scanner sc, List<String> listaPalabras) {
+    public LogicKwic(Scanner sc, List<List<String>> listaPalabras) {
         this.sc = sc;
         this.listaPalabras = listaPalabras;
 
@@ -31,26 +33,63 @@ public class LogicKwic {
         return sc.nextLine();
     }
 
-    private char pedirConfirmacion() {
+    private boolean pedirConfirmacion() {
+        char charAux;
         while (true) {
             try {
                 System.out.println("Desea continuat? y/n");
-                char aux = Character.toUpperCase(sc.nextLine().charAt(0));
-                return aux;
+                charAux = Character.toUpperCase(sc.nextLine().charAt(0));
+                
+                if(charAux == 'Y' || charAux == 'N') return charAux == 'Y';
+                
+                System.out.println("Intente de nuevo");
 
             } catch (Exception e) {
                 System.out.println("Upss. Ah ocurrido algo mal, intenta ingresar de nuevo una letra");
             }
         }
     }
+    
+    private List<String> filtrarListaPalabras(List<String> auxList){
+        List<String> auxTexto;
+        
+        auxTexto = auxList.stream()
+                .filter(actual -> !palabrasIgnoradas.contains(actual.toLowerCase()))
+                .toList();
+        
+        return auxTexto;
+    }
 
-    public List<String> listaPalabras() {
+    private List<List<String>> listaPalabras() {
+        
+        List<String> auxTexto;
+        
         do {
             texto = pedirTexto();
-            listaPalabras.add(texto);
+            auxTexto = Arrays.asList(texto.trim().split("\\s+"));
+            listaPalabras.add(filtrarListaPalabras(auxTexto));
+            
 
-        } while (pedirConfirmacion() == 'Y');
+        } while (pedirConfirmacion());
         return listaPalabras;
     }
+    
+    public List<String> generadorColas(List<List<String>> lista){
+        Queue<String> colita = new ArrayDeque<>();
+        String auxText = "";
+        
+        for (List<String> listaPalabras : lista) {
+            for (int i = 0; i < listaPalabras.size(); i++) {
+                auxText = listaPalabras.get(i);
+                colita.offer(auxText);
+                
+            }
+            
+        }
+        
+        
+        return auxList;
+    }
+    
 
 }
